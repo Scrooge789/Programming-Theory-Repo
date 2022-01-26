546cb5cad28e72b60654c267d7ec125090474afb
Oscar <oscar.freyer@googlemail.com>
2022-01-26 17:11:04 +0100

    finished main menu

---
+++ Top Gun/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public string playerName;
+
+    private void Awake()
+    {
+        //This pattern is a singleton. Use it to ensure that only a single instance of the MainManager can ever exist, so it acts as a central point of access.
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+    }
+}
---
+++ Top Gun/Assets/Scripts/UI/MenuUIHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUIHandler : MonoBehaviour
+{
+    [SerializeField] InputField userNameInputText;
+
+    public void StartNew()
+    {
+        GameManager.Instance.playerName = userNameInputText.GetComponent<InputField>().text;
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
